acf2c71e910af3c9ad9c582c8bb146ad8aa3b425
dashlexz1 <adr1999@icloud.com>
2024-03-21 23:47:59 +0300

    task done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mysq";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Dashlexz1!";
+
+    public static Connection connection() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("соединение есть");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("ошибка");
+        }
+        return connection;
+    }
 }
